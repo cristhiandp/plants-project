0978c40fa4672fbe475e6f687a075feb0bdcba27
Cristhian <ccdelgadop@gmail.com>
2023-11-05 22:45:03 -0500

    remove list

--- src/app/plant/plant.module.ts
+++ src/app/plant/plant.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlantsListComponent } from './components/plants-list/plants-list.component';
-import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [PlantsListComponent],
